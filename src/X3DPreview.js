18b040688b38f0ff602b2176b2c9768353ddb0a8
Holger Seelig <holger.seelig@gmail.com>
2024-10-01 18:19:04 +0200

    Open preview.

--- src/X3DPreview.js
+++ src/X3DPreview.js
@@ -1,13 +1,81 @@
+const
+   vscode = require ("vscode"),
+   path   = require ("path");
+
 class X3DPreview
 {
+   #context;
+   #panels = new Map ();
+
    constructor (context)
    {
       console .log ("Constructed preview.");
+
+      this .#context = context;
    }
 
    openPanel (textEditor)
    {
       console .log ("Called openPanel.");
+
+      const
+         filePath = textEditor .document .fileName,
+         content  = textEditor .document .getText ();
+
+      const panel = this .#panels .get (filePath)
+         ?? this .createPanel (textEditor);
+
+      this .updatePanel (panel, filePath, content);
+
+      panel .reveal (vscode .ViewColumn .Two);
+
+      // this .setActivePanel (panel);
+   }
+
+   createPanel (textEditor)
+   {
+      const
+         filePath = textEditor .document .fileName,
+         basename = path .basename (filePath);
+
+      const localResourceRoots = [
+         vscode .Uri .file (this .#context .extensionPath),
+      ];
+
+      const panel = vscode .window .createWebviewPanel ("x3d.preview", "X3D Preview", vscode .ViewColumn .Two,
+      {
+         enableScripts: true,
+         retainContextWhenHidden: true,
+         localResourceRoots: localResourceRoots,
+      });
+
+      panel .title      = `X3D Preview [${basename}]`;
+      panel .watchers   = [ ];
+      panel .textEditor = textEditor;
+
+      // panel .onDidDispose (() =>
+      // {
+      //    this .unwatchFiles (panel);
+      //    this .#panels .delete (filePath);
+      //    this .updateActivePanel ();
+      // });
+
+      // panel .onDidChangeViewState (() => this .updateActivePanel ());
+
+      this .#panels .set (filePath, panel);
+
+      return panel;
+   }
+
+   updatePanel (panel, filePath, content)
+   {
+      // const basename = path .basename (filePath);
+
+      // panel .webview .html = this .formatHtml (panel, content, basename) .replace(/\${webview.cspSource}/g, panel .webview .cspSource);
+
+      // panel .webview .onDidReceiveMessage (message => this .onDidReceiveMessage (panel, message));
+
+      // this .watchFiles (panel);
    }
 }
 
--- src/extension.js
+++ src/extension.js
@@ -41,14 +41,14 @@ function activate (context)
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode .commands .registerCommand ("x_ite-vscode.preview-model", function ()
+	const disposable = vscode .commands .registerCommand ("x_ite-vscode.preview-model", () =>
 	{
 		// The code you place here will be executed every time your command is executed
 
 		const textEditor = vscode .window .activeTextEditor;
 
-		// if (!checkValidEditor (textEditor))
-		// 	return;
+		if (!checkValidEditor (textEditor))
+			return;
 
 		// // Display a message box to the user
 		// vscode .window .showInformationMessage ("Running 'X3D: Preview 3D Model'.");
