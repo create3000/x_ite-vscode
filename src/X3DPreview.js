a6757ff631c060e77bf5a18002f0ffd27fa15295
Holger Seelig <holger.seelig@gmail.com>
2024-10-01 20:02:40 +0200

    Send messages.

--- src/X3DPreview.js
+++ src/X3DPreview.js
@@ -51,9 +51,10 @@ class X3DPreview
          localResourceRoots: localResourceRoots,
       });
 
-      panel .title      = `X3D Preview [${basename}]`;
-      panel .watchers   = [ ];
-      panel .textEditor = textEditor;
+      panel .title         = `X3D Preview [${basename}]`;
+      panel .watchers      = [ ];
+      panel .textEditor    = textEditor;
+      panel .webview .html = this .getWebviewContent (panel);
 
       panel .onDidDispose (() =>
       {
@@ -69,53 +70,65 @@ class X3DPreview
       return panel;
    }
 
-   updatePanel (panel, filePath, content)
-   {
-      // const basename = path .basename (filePath);
-
-      panel .webview .html = this .getWebviewContent (panel, filePath);
-
-      // panel .webview .onDidReceiveMessage (message => this .onDidReceiveMessage (panel, message));
-
-      // this .watchFiles (panel);
-   }
-
-   getWebviewContent (panel, filePath)
+   updatePanel (panel, filePath)
    {
       const src = panel .webview .asWebviewUri (vscode .Uri .file (filePath));
 
+      panel .webview .postMessage ({ command: "load-url", src: src .toString () });
+   }
+
+   getWebviewContent (panel)
+   {
       return /* html */ `<!DOCTYPE html>
-         <html>
-         <head>
-            <meta charset="utf-8">
-            <script type="module">
-               import X3D from "https://cdn.jsdelivr.net/npm/x_ite@latest/dist/x_ite.min.mjs";
+<html>
+<head>
+   <meta charset="utf-8">
+   <script type="module">
+import X3D from "https://cdn.jsdelivr.net/npm/x_ite@latest/dist/x_ite.min.mjs";
 
-               const Browser = X3D .getBrowser ();
+const Browser = X3D .getBrowser ();
 
-               Browser .getContextMenu () .setUserMenu (() =>
-               {
-                  return {
-                     "viewAll": {
-                        "name": "View All",
-                        "callback": () => Browser .viewAll (),
-                     },
-                  };
-               });
-            </script>
-            <style>
-               html, body, x3d-canvas {
-                  margin: 0;
-                  padding: 0;
-                  width: 100vw;
-                  height: 100vh;
-               }
-            </style>
-         </head>
-         <body>
-            <x3d-canvas src="${src}" debug="true" contentScale="auto"></x3d-canvas>
-         </body>
-         </html>`;
+Browser .getContextMenu () .setUserMenu (() =>
+{
+   return {
+      "viewAll": {
+         "name": "View All",
+         "callback": () => Browser .viewAll (),
+      },
+   };
+});
+
+window .addEventListener ("message", event =>
+{
+   const data = event .data;
+
+   switch (data .command)
+   {
+      case "load-url":
+      {
+         Browser .loadURL (new X3D .MFString (data .src));
+         break;
+      }
+   }
+});
+   </script>
+   <style>
+html, body, x3d-canvas {
+   margin: 0;
+   padding: 0;
+   width: 100vw;
+   height: 100vh;
+}
+   </style>
+</head>
+<body>
+   <x3d-canvas
+         debug="true"
+         splashScreen="false"
+         contentScale="auto">
+   </x3d-canvas>
+</body>
+</html>`;
    }
 }
 
