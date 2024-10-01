641eec9b32ac2af9f79c2205d10d6e846c922227
Holger Seelig <holger.seelig@gmail.com>
2024-10-01 16:31:23 +0200

    Removed spaces.

--- extension.js
+++ extension.js
@@ -10,7 +10,6 @@ const vscode = require ('vscode');
  */
 function activate (context)
 {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console .log ('Congratulations, your extension "x-ite-vscode-extension" is now active!');
