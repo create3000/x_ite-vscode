81c28f247c0d91715d2021b2fa793a63d16870d2
Holger Seelig <holger.seelig@gmail.com>
2024-10-11 17:11:44 +0200

    Restore Toolbar states.

--- src/X3DPreview.js
+++ src/X3DPreview.js
@@ -69,7 +69,7 @@ class X3DPreview
          .appendTo (toolbar);
 
       const grip = $("<button></button>")
-         .attr ("title", "Reveal Toolbar.")
+         .attr ("title", "Reveal/Hide Toolbar.")
          .addClass (["fa-solid", "fa-grip-lines-vertical", "grip"])
          .on ("click", () =>
          {
