123f2a30db5c11d3f14ded3e138e74540876fa16
Holger Seelig <holger.seelig@gmail.com>
2025-11-27 08:13:49 +0100

    Fixed spaces.

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,8 @@
 // The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const
-	vscode    = require ("vscode"),
-	X3DWindow = require ("./X3DWindow");
+   vscode    = require ("vscode"),
+   X3DWindow = require ("./X3DWindow");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,27 +12,27 @@ const
  */
 function activate (context)
 {
-	const x3dWindow = new X3DWindow (context);
+   const x3dWindow = new X3DWindow (context);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode .commands .registerCommand ("x_ite-vscode.preview-model", () =>
-	{
-		// The code you place here will be executed every time your command is executed
+   // The command has been defined in the package.json file
+   // Now provide the implementation of the command with  registerCommand
+   // The commandId parameter must match the command field in package.json
+   const disposable = vscode .commands .registerCommand ("x_ite-vscode.preview-model", () =>
+   {
+      // The code you place here will be executed every time your command is executed
 
-		const textEditor = vscode .window .activeTextEditor;
+      const textEditor = vscode .window .activeTextEditor;
 
-		if (!textEditor ?.document)
-			return;
+      if (!textEditor ?.document)
+         return;
 
-		// // Display a message box to the user
-		// vscode .window .showInformationMessage ("Running 'X3D: Preview 3D Model'.");
+      // // Display a message box to the user
+      // vscode .window .showInformationMessage ("Running 'X3D: Preview 3D Model'.");
 
-		x3dWindow .panel .openPreview (textEditor);
-	});
+      x3dWindow .panel .openPreview (textEditor);
+   });
 
-	context .subscriptions .push (disposable);
+   context .subscriptions .push (disposable);
 }
 
 // This method is called when your extension is deactivated
@@ -42,6 +42,6 @@ function deactivate ()
 }
 
 module .exports = {
-	activate,
-	deactivate,
+   activate,
+   deactivate,
 };
